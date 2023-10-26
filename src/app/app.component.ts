a2888b380dbfed0b63039d136ee1badc8cf007a6
Madhusudan-Mishra <madhusudan.mishra@comprotechnologies.com>
2023-10-26 19:57:04 +0530

    Added custom plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const STABLE_FEATURE: boolean;
+declare const EXPERIMENTAL_FEATURE: boolean;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'customWebpackPluginCLI';
+  stableFeature: string;
+  experimentalFeature: string;
+  ngOnInit(): void {
+    this.stableFeature = STABLE_FEATURE ? 'Stable feature enabled' : 'Stable feature disabled';
+    this.experimentalFeature = EXPERIMENTAL_FEATURE ? 'Experimental feature enabled' : 'Experimental feature disabled';
+    console.log('Test ' + this.stableFeature);
+    console.log('Environment :', this.experimentalFeature);
+  }
 }
